c2f53279debaaabaf4db08d4a8df7a3a9df22784
Johan Ostrand <muztanger@gmail.com>
2022-12-01 22:54:28 +0100

    Added Box type

---
+++ Advent of Code 2022/common/Box.cs
@@ -0,0 +1,36 @@
+﻿namespace Advent_of_Code_2022.common;
+
+public class Box<T> where T : INumber<T>
+{
+    public Pos<T> UpperLeft { get; set; }
+    public Pos<T> LowerRight { get; set; }
+
+    public T Width => T.Abs(LowerRight.x - UpperLeft.x);
+    public T Height => T.Abs(LowerRight.y - UpperLeft.y);
+
+    public Box(Pos<T> upperLeft, Pos<T> lowerRight) 
+    {
+        if (upperLeft.x > lowerRight.x) throw new ArgumentException("upperLeft is right of lowerRight");
+        if (upperLeft.y > lowerRight.y) throw new ArgumentException("upperLeft is below lowerRight");
+        UpperLeft = upperLeft;
+        LowerRight = lowerRight;
+    }
+
+    public override string ToString()
+    {
+        return $"[{UpperLeft}, {LowerRight}]";
+    }
+
+    public bool IsInside(Pos<T> pos)
+    {
+        if (pos.x < UpperLeft.x || pos.x > LowerRight.x)
+        {
+            return false;
+        }
+        if (pos.y < UpperLeft.y || pos.y > LowerRight.y)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Advent of Code 2022/common/TestCommon.cs
@@ -0,0 +1,41 @@
+﻿namespace Advent_of_Code_2022.common;
+
+[TestClass]
+public class TestCommon
+{
+    [TestMethod]
+    public void TestBox()
+    {
+        // Test Constructor
+        Assert.ThrowsException<ArgumentException>(() => new Box<int>(new Pos<int>(3, 3), new Pos<int>(0, 0)));
+        Assert.ThrowsException<ArgumentException>(() => new Box<int>(new Pos<int>(3, 0), new Pos<int>(2, 3)));
+        Assert.ThrowsException<ArgumentException>(() => new Box<int>(new Pos<int>(0, 3), new Pos<int>(3, 2)));
+
+        var window = new Box<int>(new Pos<int>(0, 0), new Pos<int>(3, 3));
+        
+        // Test IsInside
+        Assert.IsTrue(window.IsInside(new Pos<int>(1, 1)));
+
+        for (int x = window.UpperLeft.x; x <= window.LowerRight.x; x++)
+        {
+            for (int y = window.UpperLeft.y; y <= window.LowerRight.y; y++)
+            {
+                Assert.IsTrue(window.IsInside(new Pos<int>(x, y)));
+            }
+        }
+
+        Assert.IsFalse(window.IsInside(new Pos<int>(-1, 1)));
+        Assert.IsFalse(window.IsInside(new Pos<int>(4, 1)));
+        Assert.IsFalse(window.IsInside(new Pos<int>(1, -1)));
+        Assert.IsFalse(window.IsInside(new Pos<int>(1, 4)));
+
+        // Test Width
+        Assert.AreEqual(3, new Box<int>(new Pos<int>(-1, 0), new Pos<int>(2, 0)).Width);
+        Assert.AreEqual(2, new Box<int>(new Pos<int>(1, 0), new Pos<int>(3, 0)).Width);
+
+        // Test Height
+        Assert.AreEqual(3, new Box<int>(new Pos<int>(0, -1), new Pos<int>(0, 2)).Height);
+        Assert.AreEqual(2, new Box<int>(new Pos<int>(0, 1), new Pos<int>(0, 3)).Height);
+    }
+    
+}

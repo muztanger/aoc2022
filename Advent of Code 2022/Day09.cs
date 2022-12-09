1d1d66bf680782cae64b871f7902091dfabfd4e1
Johan Ostrand <muztanger@gmail.com>
2022-12-09 20:18:43 +0100

    Day9 cleanup
    Removed prints since they took 90% of the execution time

--- Advent of Code 2022/Day09.cs
+++ Advent of Code 2022/Day09.cs
@@ -3,92 +3,18 @@ namespace Advent_of_Code_2022;
 [TestClass]
 public class Day09
 {
-    private static string Part1(IEnumerable<string> input)
-    {
-        var result = new StringBuilder();
-        var h = new Pos<int>(0, 0);
-        var t = new Pos<int>(0, 0);
-        var counts = new Dictionary<Pos<int>, int>();
-        counts[h] = 1;
-        var dir = new Dictionary<char, Pos<int>> { { 'R', new(1, 0) }, { 'L', new(-1, 0) }, { 'U', new(0, -1) }, { 'D', new(0, 1) } };
-        var box = new Box<int>(new Pos<int>(0, -4), new Pos<int>(4, 0));
-        void Print(Pos<int> h, Pos<int> t)
-        {
-            for (int y = -5; y <= 5; y++)
-            {
-                var line = new StringBuilder();
-                for (int x = -5; x <= 5; x++)
-                {
-                    var p = new Pos<int>(x, y);
-                    if (p == h)
-                    {
-                        line.Append('H');
-                    }
-                    else if (p == t)
-                    {
-                        line.Append('T');
-                    }
-                    else
-                    {
-                        line.Append('.');
-                    }
-                }
-                Console.WriteLine(line);
-            }
-            Console.WriteLine();
-        }
-        Print(h, t);
-        foreach (var line in input)
-        {
-            Console.WriteLine(line);
-            var (moveStr, moveCountStr) = line.Split();
-            var moveCount = int.Parse(moveCountStr);
-            var move = dir[moveStr[0]];
-            for (int i = 0; i < moveCount; i++)
-            {
-                h += move;
-                if (!t.Adjacent(h))
-                {
-                    var candidates = new List<(int, Pos<int>)>();
-                    foreach (var dt in new List<Pos<int>>() {
-                        new(0, 1),
-                        new(1, 0),
-                        new(1, 1),
-                        new(0, -1),
-                        new(-1, 0),
-                        new(-1, -1),
-                        new(1, -1),
-                        new(-1, 1),
-                    })
-                    {
-                        var nt = t + dt;
-                        if (nt.Adjacent(h))
-                        {
-                            candidates.Add((h.Manhattan(nt), nt));
-                        }
-                    }
-                    t = candidates.OrderBy(x => x.Item1).First().Item2;
-                    counts.TryGetValue(t, out int count);
-                    count += 1;
-                    counts[t] = count;
-                }
-                Print(h, t);
-            }
-        }
-        return counts.Count.ToString();
-    }
-    
-    private static string Part2(IEnumerable<string> input)
+    private static string Calc(IEnumerable<string> input, int N, bool isDebug = false)
     {
         var result = new StringBuilder();
         var s = new Pos<int>(0, 0);
-        var ts = Enumerable.Repeat(s, 10).ToList();
+        var ts = Enumerable.Repeat(s, N).ToList();
         var counts = new Dictionary<Pos<int>, int>();
         counts[s] = 1;
         var dir = new Dictionary<char, Pos<int>> { { 'R', new(1, 0) }, { 'L', new(-1, 0) }, { 'U', new(0, -1) }, { 'D', new(0, 1) } };
         var box = new Box<int>(new Pos<int>(0, -4), new Pos<int>(4, 0));
         void Print(List<Pos<int>> ts)
         {
+            if (!isDebug) return;
             for (int y = -16; y <= 12; y++)
             {
                 var line = new StringBuilder();
@@ -132,7 +58,7 @@ public class Day09
         Print(ts);
         foreach (var line in input)
         {
-            Console.WriteLine(line);
+            if (isDebug) Console.WriteLine(line);
             var (moveStr, moveCountStr) = line.Split();
             var moveCount = int.Parse(moveCountStr);
             var move = dir[moveStr[0]];
@@ -177,9 +103,12 @@ public class Day09
             }
             Print(ts);
         }
-        foreach (var c in counts)
+        if (isDebug)
         {
-            Console.WriteLine($"{c.Key}: {c.Value}");
+            foreach (var c in counts)
+            {
+                Console.WriteLine($"{c.Key}: {c.Value}");
+            }
         }
         return counts.Count.ToString();
     }
@@ -198,14 +127,14 @@ public class Day09
             L 5
             R 2
             """;
-        var result = Part1(Common.GetLines(input));
+        var result = Calc(Common.GetLines(input), 2);
         Assert.AreEqual("13", result);
     }
     
     [TestMethod]
     public void Day09_Part1()
     {
-        var result = Part1(Common.DayInput(nameof(Day09)));
+        var result = Calc(Common.DayInput(nameof(Day09)), 2);
         Assert.AreEqual("6023", result);
     }
     
@@ -222,7 +151,7 @@ public class Day09
             L 5
             R 2
             """;
-        var result = Part2(Common.GetLines(input));
+        var result = Calc(Common.GetLines(input), 10);
         Assert.AreEqual("1", result);
     }
     
@@ -239,14 +168,14 @@ public class Day09
             L 25
             U 20
             """;
-        var result = Part2(Common.GetLines(input));
+        var result = Calc(Common.GetLines(input), 10);
         Assert.AreEqual("36", result);
     }
     
     [TestMethod]
     public void Day09_Part2()
     {
-        var result = Part2(Common.DayInput(nameof(Day09)));
+        var result = Calc(Common.DayInput(nameof(Day09)), 10);
         Assert.AreEqual("2533", result);
     }
     

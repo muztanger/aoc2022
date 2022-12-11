f5dee98719236b52f4c8842a618ef689d62b582e
Johan Ostrand <muztanger@gmail.com>
2022-12-11 12:05:59 +0100

    Day11 part2 tooo slow

--- Advent of Code 2022/Day11.cs
+++ Advent of Code 2022/Day11.cs
@@ -23,7 +23,7 @@ public class Day11
             }
         }
 
-        var monkeyBusiness = new List<long>();
+        var monkeyBusiness = new List<BigInteger>();
         for (int i = 0; i < monkeys.Count; i++)
         {
             //Monkey 0 inspected items 101 times.
@@ -31,36 +31,57 @@ public class Day11
             monkeyBusiness.Add(monkeys[i].Inspections);
         }
 
-        return monkeyBusiness.OrderByDescending(x => x).Take(2).Aggregate(1L, (a, b) => a * b).ToString();
+        return monkeyBusiness.OrderByDescending(x => x).Take(2).Aggregate(BigInteger.One, (a, b) => a * b).ToString();
     }
 
     private static string Part2(IEnumerable<string> input)
     {
-        return "meep";
+        var monkeys = Monkey.Parse(input);
+        var game = new Game(monkeys);
+        Console.WriteLine(game);
+        Console.WriteLine();
+        var check = new List<int> { 1, 20, 1000, 2000, 3000, 7000, 9000, 10000 };
+        for (int round = 1; round <= 20; round++)
+        {
+            game.Round(canRelax: false);
+            if (check.Contains(round))
+            {
+                //Console.WriteLine($"After round: {round}");
+                //Console.WriteLine(game);
+                for (int i = 0; i < monkeys.Count; i++)
+                {
+                    //Monkey 0 inspected items 101 times.
+                    Console.WriteLine($"Monkey {i} inspected items {monkeys[i].Inspections} times.");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        return monkeys.Select(x => x.Inspections).OrderByDescending(x => x).Take(2).Aggregate(BigInteger.One, (a, b) => a * b).ToString();
     }
 
     public class Monkey
     {
-        private long _testVal;
+        private BigInteger _testVal;
         private int _trueAction;
         private int _falseAction;
 
-        public List<long> StartingItems { get; private set; } = new List<long>();
+        public List<BigInteger> StartingItems { get; private set; } = new List<BigInteger>();
         public char Op { get; private set; }
-        public long? OpLeft { get; private set; } = null;
-        public long? OpRight { get; private set; } = null;
-        public long Inspections { get; private set; } = 0L;
+        public BigInteger? OpLeft { get; private set; } = null;
+        public BigInteger? OpRight { get; private set; } = null;
+        public BigInteger Inspections { get; private set; } = 0L;
 
-        public List<(long, int)> Turn()
+        public List<(BigInteger, int)> Turn(bool canRelax = true)
         {
-            var throws = new List<(long, int)>();
+            var throws = new List<(BigInteger, int)>();
             for (int i = 0; i < StartingItems.Count; i++)
             {
                 Inspections++;
 
                 // inspect
-                long left = StartingItems[i];
-                long right = StartingItems[i];
+                BigInteger left = StartingItems[i];
+                BigInteger right = StartingItems[i];
                 if (OpLeft.HasValue)
                 {
                     left = OpLeft.Value;
@@ -69,6 +90,13 @@ public class Day11
                 {
                     right = OpRight.Value;
                 }
+
+                //if (!canRelax)
+                //{
+                //    left %= _testVal;
+                //    right %= _testVal;
+                //}
+
                 if (Op == '+')
                 {
                     StartingItems[i] = left + right;
@@ -83,7 +111,14 @@ public class Day11
                 }
 
                 // relax
-                StartingItems[i] /= 3L;
+                if (canRelax)
+                {
+                    StartingItems[i] /= 3L;
+                }
+                else
+                {
+                    //StartingItems[i] %= _testVal;
+                }
 
                 // throw
                 var throwTo = StartingItems[i] % _testVal == 0 ? _trueAction : _falseAction;
@@ -93,7 +128,7 @@ public class Day11
             return throws;
         }
 
-        public void Receive(long item)
+        public void Receive(BigInteger item)
         {
             StartingItems.Add(item);
         }
@@ -117,25 +152,25 @@ public class Day11
                     }
                     else if (trimmedLine.StartsWith("Starting items:"))
                     {
-                        var items = trimmedLine["Starting items: ".Length..].Split(",").Select(x => long.Parse(x)).ToList();
+                        var items = trimmedLine["Starting items: ".Length..].Split(",").Select(x => BigInteger.Parse(x)).ToList();
                         monkey.StartingItems = items;
                     }
                     else if (trimmedLine.StartsWith("Operation:"))
                     {
                         var (left, op, right) = trimmedLine.Split()[^3..];
                         monkey.Op = op[0];
-                        if (long.TryParse(left, out var leftValue))
+                        if (BigInteger.TryParse(left, out var leftValue))
                         {
                             monkey.OpLeft = leftValue;
                         }
-                        if (long.TryParse(right, out var rightValue))
+                        if (BigInteger.TryParse(right, out var rightValue))
                         {
                             monkey.OpRight = rightValue;
                         }
                     }
                     else if (trimmedLine.StartsWith("Test:"))
                     {
-                        monkey._testVal = long.Parse(trimmedLine.Split().Last());
+                        monkey._testVal = BigInteger.Parse(trimmedLine.Split().Last());
                     }
                     else if (trimmedLine.StartsWith("If true:"))
                     {
@@ -164,11 +199,11 @@ public class Day11
             this._monkeys = monkeys;
         }
 
-        public void Round()
+        public void Round(bool canRelax = true)
         {
             for (int monkey = 0; monkey < _monkeys.Count; monkey++)
             {
-                var throws = _monkeys[monkey].Turn();
+                var throws = _monkeys[monkey].Turn(canRelax);
                 foreach (var t in throws)
                 {
                     _monkeys[t.Item2].Receive(t.Item1);
@@ -193,76 +228,54 @@ public class Day11
         }
     }
     
+    string _example = """
+        Monkey 0:
+            Starting items: 79, 98
+            Operation: new = old * 19
+            Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+
+        Monkey 1:
+            Starting items: 54, 65, 75, 74
+            Operation: new = old + 6
+            Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+
+        Monkey 2:
+            Starting items: 79, 60, 97
+            Operation: new = old * old
+            Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+
+        Monkey 3:
+            Starting items: 74
+            Operation: new = old + 3
+            Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
+        """;
+
     [TestMethod]
     public void Day11_Part1_Example01()
     {
-        var input = """
-            Monkey 0:
-              Starting items: 79, 98
-              Operation: new = old * 19
-              Test: divisible by 23
-                If true: throw to monkey 2
-                If false: throw to monkey 3
-
-            Monkey 1:
-              Starting items: 54, 65, 75, 74
-              Operation: new = old + 6
-              Test: divisible by 19
-                If true: throw to monkey 2
-                If false: throw to monkey 0
-
-            Monkey 2:
-              Starting items: 79, 60, 97
-              Operation: new = old * old
-              Test: divisible by 13
-                If true: throw to monkey 1
-                If false: throw to monkey 3
-
-            Monkey 3:
-              Starting items: 74
-              Operation: new = old + 3
-              Test: divisible by 17
-                If true: throw to monkey 0
-                If false: throw to monkey 1
-            """;
-        var result = Part1(Common.GetLines(input));
-        Assert.AreEqual("", result);
+        var result = Part1(Common.GetLines(_example));
+        Assert.AreEqual("10605", result);
     }
-    
-    [TestMethod]
-    public void Day11_Part1_Example02()
-    {
-        var input = """
-            <TODO>
-            """;
-        var result = Part1(Common.GetLines(input));
-        Assert.AreEqual("", result);
-    }
-    
+        
     [TestMethod]
     public void Day11_Part1()
     {
         var result = Part1(Common.DayInput(nameof(Day11)));
-        Assert.AreEqual("", result);
+        Assert.AreEqual("56120", result);
     }
     
     [TestMethod]
     public void Day11_Part2_Example01()
     {
-        var input = """
-            <TODO>
-            """;
-        var result = Part2(Common.GetLines(input));
-        Assert.AreEqual("", result);
-    }
-    
-    [TestMethod]
-    public void Day11_Part2_Example02()
-    {
-        var input = """
-            <TODO>
-            """;
-        var result = Part2(Common.GetLines(input));
+        var result = Part2(Common.GetLines(_example));
         Assert.AreEqual("", result);
     }
     

15b6e263ba37a5ba7ed13503b2d3f434d0fa69b7
Johan Ostrand <muztanger@gmail.com>
2022-12-13 22:44:44 +0100

    Day13 parser seems ok-ish

--- Advent of Code 2022/Day13.cs
+++ Advent of Code 2022/Day13.cs
@@ -1,66 +1,111 @@
+using System.ComponentModel;
+
 namespace Advent_of_Code_2022;
 
-
-enum Type { Integer, List};
 
 [TestClass]
 public class Day13
 {
     
-    record Item(Type type, int X, List<int> List);
+    public class Item
+    {
+        public Item? Parent { get; set; } = null;
+
+        public Item(Item? parent = null)
+        {
+            Parent = parent;
+        }
+    }
+
+    public class IntItem : Item
+    {
+        public int X { get; set; } = -1;
+        public IntItem() { }
+        public override string ToString()
+        {
+            return X.ToString();
+        }
+    }
+
+    public class ListItem : Item
+    {
+        public List<Item> Items { get; set; } = new();
+        public ListItem() { }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", Items)}]";
+        }
+    }
 
     private static string Part1(IEnumerable<string> input)
     {
         var result = new StringBuilder();
         
-        var signals = new List<List<Item>>();
         var index = 0;
         foreach (var line in input)
         {
             if (string.IsNullOrEmpty(line)) continue;
 
-            var stack = new Stack<(List<Item>, Type)>();
-            var list = new List<Item>();
+            var lists = new List<Item>();
+            Item root = new ListItem();
+            var current = root;
             var val = new StringBuilder();
-            var type = Type.List;
+            var stack = new Stack<Item>();
             foreach (var c in line)
             {
+                Assert.IsTrue(current != null);
                 switch (c)
                 {
                     case '[':
-                        stack.Push((list, type));
-                        list = new List<Item>();
-                        type = Type.List;
+                        current = new ListItem() { Parent = current };
                         break;
                     case ']':
-                        // end number or end list
-                        if (type == Type.Integer)
                         {
-                            list.Add(new Item(Type.Integer, int.Parse(val.ToString()), new()));
-                            val.Clear();
+                            if (current is IntItem integer)
+                            {
+                                integer.X = int.Parse(val.ToString());
+                                val.Clear();
+                            }
+                            if (current.Parent is ListItem list)
+                            {
+                                list.Items.Add(current);
+                            }
+                            current = current.Parent;
                         }
-                        signals.Add(list);
-                        (list, type) = stack.Pop();
                         break;
                     case ',':
-                        // end number or end list
-                        if (type == Type.Integer)
                         {
-                            list.Add(new Item(Type.Integer, int.Parse(val.ToString()), new()));
-                            val.Clear();
+                            if (current is IntItem integer)
+                            {
+                                integer.X = int.Parse(val.ToString());
+                                val.Clear();
+                            }
+                            if (current.Parent is ListItem list)
+                            {
+                                list.Items.Add(current);
+                            }
+                            current = current.Parent;
                         }
                         break;
                     case char d when d >= '0' && d <= '9':
                         val.Append(c);
-                        type = Type.Integer;
+                        if (current is not IntItem)
+                        {
+                            current = new IntItem() { Parent = current };
+                        }
                         break;
                     default:
-                        Assert.Fail();
+                        Assert.Fail(c.ToString());
                         break;
                 }
             }
+            if (current.Parent is ListItem parentWithList)
+            {
+                parentWithList.Items.Add(current);
+            }
 
-            //signals[index % 2] = 
+            //lists[index % 2] = 
 
             index++;    
         }
@@ -80,7 +125,29 @@ public class Day13
     public void Day13_Part1_Example01()
     {
         var input = """
-            <TODO>
+            [1,1,3,1,1]
+            [1,1,5,1,1]
+
+            [[1],[2,3,4]]
+            [[1],4]
+
+            [9]
+            [[8,7,6]]
+
+            [[4,4],4,4]
+            [[4,4],4,4,4]
+
+            [7,7,7,7]
+            [7,7,7]
+
+            []
+            [3]
+
+            [[[]]]
+            [[]]
+
+            [1,[2,[3,[4,[5,6,7]]]],8,9]
+            [1,[2,[3,[4,[5,6,0]]]],8,9]
             """;
         var result = Part1(Common.GetLines(input));
         Assert.AreEqual("", result);

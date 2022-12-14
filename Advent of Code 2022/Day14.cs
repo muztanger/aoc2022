186cc602e704effc8d8aeadd02e4bb5c7f56d98b
Johan Ostrand <muztanger@gmail.com>
2022-12-15 00:00:10 +0100

    Day14 finished

--- Advent of Code 2022/Day14.cs
+++ Advent of Code 2022/Day14.cs
@@ -77,47 +77,129 @@ public class Day14
             //PrintCave(units, area, sands);
         }
 
-        return (sands.Count - 1).ToString();
+        PrintCave(units, area, sands);
 
-        static void PrintCave(Dictionary<Pos<int>, char> units, Box<int> area, HashSet<Pos<int>> sands)
+        return (sands.Count - 1).ToString();
+    }
+
+    private static void PrintCave(Dictionary<Pos<int>, char> units, Box<int> area, HashSet<Pos<int>> sands)
+    {
+        var cave = new StringBuilder();
+        for (int y = area.UpperLeft.y; y <= area.LowerRight.y; y++)
         {
-            var cave = new StringBuilder();
-            for (int y = area.UpperLeft.y; y <= area.LowerRight.y; y++)
+            if (y != area.UpperLeft.y)
             {
-                if (y != area.UpperLeft.y)
+                cave.AppendLine();
+            }
+            for (int x = area.UpperLeft.x; x <= area.LowerRight.x; x++)
+            {
+                var p = new Pos<int>(x, y);
+                if (units.TryGetValue(p, out var c))
                 {
-                    cave.AppendLine();
+                    cave.Append(c);
                 }
-                for (int x = area.UpperLeft.x; x <= area.LowerRight.x; x++)
+                else if (sands.Contains(p))
                 {
-                    var p = new Pos<int>(x, y);
-                    if (units.TryGetValue(p, out var c))
-                    {
-                        cave.Append(c);
-                    }
-                    else if (sands.Contains(p))
-                    {
-                        cave.Append('o');
-                    }
-                    else
-                    {
-                        cave.Append('.');
-                    }
+                    cave.Append('o');
+                }
+                else
+                {
+                    cave.Append('.');
                 }
             }
-            Console.WriteLine(cave);
         }
+        Console.WriteLine(cave);
     }
 
     private static string Part2(IEnumerable<string> input)
     {
-        var result = new StringBuilder();
+        var units = new Dictionary<Pos<int>, char>();
+
+        var start = new Pos<int>(500, 0);
+        units[start] = '+';
+
+        var area = new Box<int>(start, start);
         foreach (var line in input)
         {
+            var split = line.Split(' ');
+            Pos<int>? last = null;
+            for (int i = 0; i < split.Length; i += 2)
+            {
+                var P = split[i].Split(',');
+                var p = new Pos<int>(int.Parse(P[0]), int.Parse(P[1]));
+                if (last is not null)
+                {
+                    var dp = last.Sign(p);
+                    var q = last + dp;
+                    while (q != p)
+                    {
+                        units[q] = '#';
+                        area.IncreaseToPoint(q);
+                        q += dp;
+                    }
+                }
+                area.IncreaseToPoint(p);
+                units[p] = '#';
+                last = p;
+            }
         }
-        return result.ToString();
+        var sands = new HashSet<Pos<int>>();
+        PrintCave(units, area, sands);
+
+        var floor = area.LowerRight.y + 1;
+
+        var sandFall = true;
+        while (sandFall)
+        {
+
+            var sand = start;
+            var tries = new List<Pos<int>> { new(0, 1), new(-1, 1), new(1, 1) };
+            var hasMoved = true;
+            var moveCount = 0;
+            while (hasMoved)
+            {
+                hasMoved = false;
+                foreach (var dp in tries)
+                {
+                    var q = sand + dp;
+                    if (sands.Contains(q))
+                    {
+                        continue;
+                    }
+                    if (units.ContainsKey(q))
+                    {
+                        continue;
+                    }
+                    if (sand.y == floor)
+                    {
+                        // hit the floor
+                        break;
+                    }
+                    sand = q;
+                    hasMoved = true;
+                    moveCount++;
+                    break;
+                }
+            }
+            sands.Add(sand);
+            area.IncreaseToPoint(sand);
+
+            if (sand == start)
+            {
+                // Now it is safe!
+                sandFall = false;
+            }
+            //Console.WriteLine();
+            //Console.WriteLine("Units of sand: " + sands.Count);
+            //PrintCave(units, area, sands);
+        }
+
+        PrintCave(units, area, sands);
+
+        return (sands.Count).ToString();
     }
-    
+
+
     [TestMethod]
     public void Day14_Part1_Example01()
     {
@@ -150,7 +232,8 @@ public class Day14
     public void Day14_Part2_Example01()
     {
         var input = """
-            <TODO>
+            498,4 -> 498,6 -> 496,6
+            503,4 -> 502,4 -> 502,9 -> 494,9
             """;
         var result = Part2(Common.GetLines(input));
         Assert.AreEqual("", result);

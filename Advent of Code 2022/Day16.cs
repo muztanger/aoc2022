a0d740c8669b5be19438d662562b332c6b4fc302
Johan Ostrand <muztanger@gmail.com>
2023-01-12 18:56:10 +0100

    Day16 work...

--- Advent of Code 2022/Day16.cs
+++ Advent of Code 2022/Day16.cs
@@ -115,70 +115,87 @@ public class Day16
             }
             return release + max;
         }
+        record PathItem(Valve v, int tot, int t);
 
-        record StackItem(Valve valve, int time, int total, List<Valve> openValves);
+        record StackItem(Valve valve, int time, int total, List<Valve> openValves, List<PathItem> path);
 
-        internal int SearchMaxReleasePressure(int time, int maxValveCount)
+        internal int SearchMaxReleasePressure(int time, int maxValveCount, Dictionary<(Valve, Valve), int> valveDistances)
         {
             int Release(List<Valve> openValves) => openValves.Sum(v => v.FlowRate);
             var stack = new Stack<StackItem>();
             var maxTot = 0;
-            stack.Push(new StackItem(this, time, 0, new List<Valve>()));
+            var maxPath = new List<PathItem>();
+            stack.Push(new StackItem(this, time, 0, new List<Valve>(), new List<PathItem> { new(this, 0, time) }));
             while (stack.Any())
             {
                 var item = stack.Pop();
-                (var v, var t, var tot, var openValves) = item;
-                Assert.IsTrue(t >= 0, $"stack.Count={stack.Count} item={item}");
-                if (t == 0)
+                (var v, var t, var tot, var openValves, var path) = item;
+                //Assert.IsTrue(t >= 0, $"stack.Count={stack.Count} item={item}");
+                if (t < 0)
+                {
+                    // not valid
+                }
+                else if (t == 0)
                 {
                     //Console.WriteLine($"item={item} stack.Count={stack.Count}");
-                    maxTot = Math.Max(tot, maxTot);
+                    if (tot > maxTot)
+                    {
+                        maxTot = tot;
+                        maxPath = path;
+                    }
                 }
                 else if (openValves.Count == maxValveCount)
                 {
-                    t--;
                     tot += t * Release(openValves);
-                    maxTot = Math.Max(tot, maxTot);
+                    if (tot > maxTot)
+                    {
+                        maxTot = tot;
+                        maxPath = new List<PathItem>(path) { new(v, tot, 0) };
+                    }
                 }
                 else
                 {
-                    tot += Release(openValves);
-                    if (v.FlowRate > 0 && !openValves.Contains(v))
+                    // find closest closed valve
+                    foreach ((Valve next, _) in valveDistances.Keys.Where(x => x.Item2 == v && !openValves.Contains(x.Item1)))
                     {
-                        t--;
-                        var valves = new List<Valve>(openValves)
-                        {
-                            v
-                        };
-                        var next = new StackItem(v, t, tot, valves);
-                        stack.Push(next);
-                    }
-                    if (t > 0)
-                    {
-                        t--;
-                        foreach (var w in v._output.Where(x => !openValves.Contains(x)))
-                        {
-                            StackItem next = new (w, t, tot, openValves);
-                            stack.Push(next);
+                        var dist = valveDistances[(next, v)];
+                        var nextTot = tot + dist * Release(openValves);
+                        
+                        {   // open valve
+                            var nextT = t - dist - 1;
+
+                            var nextOpenValves = new List<Valve>(openValves) { next };
+                            var nextPath = new List<PathItem>(path) { new(next, nextTot, nextT)};
+                            var stackItem = new StackItem(next, nextT, nextTot, nextOpenValves, nextPath);
+                            stack.Push(stackItem);
                         }
+                        //{   // skip open valve
+                        //    var nextT = t - dist;
+
+                        //    var nextOpenValves = new List<Valve>(openValves);
+                        //    var nextPath = new List<PathItem>(path) { new(next, nextTot, nextT) };
+                        //    var stackItem = new StackItem(next, nextT, nextTot, nextOpenValves, nextPath);
+                        //    stack.Push(stackItem);
+
+                        //}
                     }
                 }
             }
-
+            var pathList = maxPath.Select(x => $"({x})");
+            Console.WriteLine($"maxPath={string.Join("\n", pathList)}");
             return maxTot;
         }
 
-        record Steps(Valve v, int x);
+        record Steps(Valve v, int x, List<Valve> path);
 
         internal int ShortestPath(Valve other)
         {
-
             var result = int.MaxValue;
             var stack = new Stack<Steps>();
-            stack.Push(new Steps(this, 0));
+            stack.Push(new Steps(this, 0, new List<Valve> { this }));
             while (stack.Any())
             {
-                (var v, var x) = stack.Pop();
+                (var v, var x, var path) = stack.Pop();
                 if (v == other)
                 {
                     result = Math.Min(result, x);
@@ -186,13 +203,13 @@ public class Day16
                 else
                 {
                     x++;
-                    if (x < result && x < 30)
+                    if (x < result)
                     {
                         foreach (var w in v._output)
                         {
-                            if (!stack.Any(x => x.v.Name == w.Name))
+                            if (!path.Contains(w))
                             {
-                                stack.Push(new Steps(w, x));
+                                stack.Push(new Steps(w, x, new List<Valve>(path) { v }));
                             }
                         }
                     }
@@ -242,7 +259,7 @@ public class Day16
             }
         }
 
-        var distances = new Dictionary<(Valve, Valve), int>();
+        var valveDistances = new Dictionary<(Valve, Valve), int>();
         foreach (var v1 in valves)
         {
             foreach (var v2 in valves)
@@ -251,21 +268,22 @@ public class Day16
                 if (v1 == root || v2 == root || (v1.FlowRate > 0 && v2.FlowRate > 0) )
                 {
                     var key = string.Join(",", (new List<string> { v1.Name, v2.Name }).OrderBy(x => x));
-                    if (!distances.TryGetValue((v1, v2), out var x))
+                    if (!valveDistances.TryGetValue((v1, v2), out var x))
                     {
-                        distances[(v1, v2)] = v1.ShortestPath(v2);
-                        distances[(v2, v1)] = distances[(v1, v2)];
+                        valveDistances[(v1, v2)] = v1.ShortestPath(v2);
+                        valveDistances[(v2, v1)] = valveDistances[(v1, v2)];
                     }
                 }
             }
         }
+
 
         var time = 30;
         var maxValveCount = valves.Count(v => v.FlowRate > 0);
         //var cache = new Dictionary<(Valve, bool, int), int>();
         //var openValves = new List<Valve>();
         //var result = root.MaxReleasedPressure(time, ref openValves, ref cache);
-        int result = root.SearchMaxReleasePressure(time, maxValveCount);
+        int result = root.SearchMaxReleasePressure(time, maxValveCount, valveDistances);
         return result.ToString();
     }
     
@@ -303,6 +321,7 @@ public class Day16
         var result = Part1(Common.DayInput(nameof(Day16)));
         Assert.AreNotEqual("164", result);
         Assert.AreNotEqual("3530", result);
+        Assert.AreNotEqual("1121", result); // too low
         Assert.AreEqual("", result);
     }
     
